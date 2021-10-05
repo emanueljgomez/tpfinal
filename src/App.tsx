8564d12284941cb17d0d411c66400ee0eba87925
Emanuel Gomez <emanueljgomez@gmail.com>
2021-10-05 16:18:38 -0300

    Added example entry

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,26 @@ import {
   WorkWeek,
   Month,
   Agenda,
+  EventSettingsModel,
 } from "@syncfusion/ej2-react-schedule";
 
 class App extends React.Component {
+  // Bind local JSON data
+  private localData: EventSettingsModel = {
+    dataSource: [
+      {
+        EndTime: new Date(2021, 9, 11, 6, 30), // Year, month (array position), day, hours, minutes
+        StartTime: new Date(2021, 9, 11, 4, 0),
+      },
+    ],
+  };
+
   public render() {
     return (
       // This is the main component of the Syncfusion React Schedule library.
       // The previously imported services are injected so they become activate,
       // rendering them in the main view and turning on their functionalities.
-      <ScheduleComponent>
+      <ScheduleComponent eventSettings={this.localData}>
         <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
       </ScheduleComponent>
     );
